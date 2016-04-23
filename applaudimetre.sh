7ccd6ee20f91ae1bac814c6bc02e17bb5b234d91
Bruno Coudoin <bruno.coudoin@gcompris.net>
2016-04-23 17:19:12 +0200

    appaudimetre, remove jo dependancy
    now the awk make the json formating by hand.

--- applaudimetre.sh
+++ applaudimetre.sh
@@ -1,2 +1,1 @@
-stdbuf -oL -eL arecord -D hw:1 -c 2 -d 10000 -f S16_LE -vvv /dev/null 2>&1 | stdbuf -oL -eL grep '%' | stdbuf -oL -eL awk -v quote="'" '{ sub(/%/, "", $7); system("jo volume="$7 " @timestamp="strftime("%FT%T,0+0200",systime()));  }' | parallel --no-notice curl -s -XPOST 'localhost:9200/record/volume' -d {} > /dev/null
-
+stdbuf -oL -eL arecord -D hw:1 -c 2 -d 10000 -f S16_LE -vvv /dev/null 2>&1 | stdbuf -oL -eL grep '%' | stdbuf -oL -eL awk -v quote="'" '{ sub(/%/, "", $7); printf "{\"volume\":%s , \"@timestamp\":\"%s\"}\n", $7, strftime("%FT%T,0+0200",systime());  }' | parallel --no-notice curl -s -XPOST 'localhost:9200/record/volume' -d {} > /dev/null
